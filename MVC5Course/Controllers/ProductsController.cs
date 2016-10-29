6ff7a78cad66febd26d78b4a21349c72db84d6ce
huayulee <huayu.lee@gmail.com>
2016-10-29 16:25:37 +0800

    44 練習用 TryUpdateModel 在 Action 裡面執行 Model Binding 並設定 includeProperties 或 excludeProperties

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -105,19 +105,33 @@ namespace MVC5Course.Controllers
         // POST: Products/Edit/5
         // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        // 使用EntityFramework
+        //        var db = this.repo.UnitOfWork.Context;
+        //        db.Entry(product).State = EntityState.Modified;
+        //        db.SaveChanges();
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(product);
+        //}
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ProductId,ProductName,Price,Active,Stock")] Product product)
+        public ActionResult Edit(int id, FormCollection form)
         {
-            if (ModelState.IsValid)
+            var product = this.repo.Find(id);
+            if (TryUpdateModel<Product>(product))
             {
-                // 使用EntityFramework
-                var db = this.repo.UnitOfWork.Context;
-                db.Entry(product).State = EntityState.Modified;
-                db.SaveChanges();
-
+                this.repo.UnitOfWork.Commit();
                 return RedirectToAction("Index");
             }
+
             return View(product);
         }
 
